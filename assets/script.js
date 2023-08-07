42eb0465dc4ae72eb508ea779b5c3b8758b0b84e
cpichac <Carlos12pichardo@outlook.com>
2023-08-07 04:07:10 -0400

    removed some empty space

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,11 @@ let cardBox = document.querySelector("#card-holder");
 let inputEl = document.querySelector("#searched-city").value;
 let timeEl  = dayjs().format('ddd MM DD, YYYY HH:mm:SS' );
 
-cityBtn.addEventListener("click", function (event) {
+cityBtn.onclick = function (event) {
     event.preventDefault();
     console.log(inputEl)
-
-    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${inputEl.value}&limit=5&appid=ddc2033f95ec7f98e923200aba974c7f`)
+// ${inputEl.value}
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=Philadelphia&limit=5&appid=ddc2033f95ec7f98e923200aba974c7f`)
         .then(response => response.json())
         .then(citiesFound => {
             let firstCity=citiesFound[0];
@@ -23,9 +23,6 @@ cityBtn.addEventListener("click", function (event) {
         .then(response => response.json())
         .then(eachDay => {
             console.log(eachDay);
-            // let searchedCity = eachDay[0].name;
-
-
 
             for (let i = 0; i < 5; i++) {
                 //Creating the elements to make the weather cards
@@ -52,7 +49,7 @@ cityBtn.addEventListener("click", function (event) {
             
 
         })
-})
+}
 
 // lastSearchedCity()
 
