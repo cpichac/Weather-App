ffd241b17eb96464b7b287a2c4e3367674f40a1f
cpichac <Carlos12pichardo@outlook.com>
2023-08-07 03:57:20 -0400

    added more code need to ask regarding getting the input to work on the fetch

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,14 @@
 
-let cityBtn = document.querySelector("#city-btn");
+let cityBtn = document.querySelector("#btn");
+let cardBox = document.querySelector("#card-holder");
+let inputEl = document.querySelector("#searched-city").value;
+let timeEl  = dayjs().format('ddd MM DD, YYYY HH:mm:SS' );
 
+cityBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log(inputEl)
 
-
-    fetch("https://api.openweathermap.org/geo/1.0/direct?q=Philadelphia&limit=5&appid=ddc2033f95ec7f98e923200aba974c7f")
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${inputEl.value}&limit=5&appid=ddc2033f95ec7f98e923200aba974c7f`)
         .then(response => response.json())
         .then(citiesFound => {
             let firstCity=citiesFound[0];
@@ -16,9 +21,42 @@ let cityBtn = document.querySelector("#city-btn");
 
 
         .then(response => response.json())
-        .then(data => {
+        .then(eachDay => {
+            console.log(eachDay);
+            // let searchedCity = eachDay[0].name;
 
 
-            console.log(data);
+
+            for (let i = 0; i < 5; i++) {
+                //Creating the elements to make the weather cards
+                let weatherCard = document.createElement("div");
+                let time = document.createElement("p");
+                let dayIcon = document.createElement("img");
+                let temp = document.createElement("p");
+
+                //adding the values to the elements
+                dayIcon.textContent = eachDay.list[i].weather[4];
+                temp.textContent = eachDay.list[i].main[0];
+
+
+                //appended elements to the div
+                cardBox.append(weatherCard);
+                weatherCard.append(dayIcon);
+                weatherCard.append(temp);
+                weatherCard.append(time);
+
+                
+            }
+
+            
+            
 
         })
+})
+
+// lastSearchedCity()
+
+// function lastSearchedCity() {
+
+    
+// }
