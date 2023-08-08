c38af303c4ab79e86cd4200e30d95e74b7a37eee
Carlos Pichardo <carlos12pichardo@outlook.com>
2023-08-07 21:32:15 -0400

    got code to generate in the cards

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,75 @@
 
 let cityBtn = document.querySelector("#btn");
 let cardBox = document.querySelector("#card-holder");
-let inputEl = document.querySelector("#searched-city").value;
-let timeEl  = dayjs().format('ddd MM DD, YYYY HH:mm:SS' );
+
+
+// cityBtn.addEventListener("click", function(event){
+
+// })
 
 cityBtn.onclick = function (event) {
+    let inputEl = document.querySelector("#searched-city").value;
+
     event.preventDefault();
     console.log(inputEl)
-// ${inputEl.value}
-    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=Philadelphia&limit=5&appid=ddc2033f95ec7f98e923200aba974c7f`)
+
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${inputEl}&limit=5&appid=ddc2033f95ec7f98e923200aba974c7f`)
+
         .then(response => response.json())
         .then(citiesFound => {
             let firstCity=citiesFound[0];
+            console.log(citiesFound);
             console.log(firstCity.lat);
             console.log(firstCity.lon);
 
-
             return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=ddc2033f95ec7f98e923200aba974c7f`)
         })
-
-
         .then(response => response.json())
-        .then(eachDay => {
+        .then(days => {
+            // console.log(days)
+            let eachDay = days.list;
             console.log(eachDay);
+            console.log(eachDay[0]);
 
-            for (let i = 0; i < 5; i++) {
+
+            for (let i = 0; i < 5; i+ 7 ) {
                 //Creating the elements to make the weather cards
                 let weatherCard = document.createElement("div");
                 let time = document.createElement("p");
                 let dayIcon = document.createElement("img");
                 let temp = document.createElement("p");
+                let cityName = document.createElement("h3");
+
+                let date = dayjs(eachDay[i].dt_txt).format("ddd MMM/DD/YYYY");
+
 
                 //adding the values to the elements
-                dayIcon.textContent = eachDay.list[i].weather[4];
-                temp.textContent = eachDay.list[i].main[0];
+                dayIcon.textContent = eachDay[i].weather[0].icon;
+                console.log(eachDay[i].weather[0].icon);
+                temp.textContent = "Temp: " + eachDay[i].main.temp;
+                console.log(eachDay[i].main.temp);
+                time.textContent = "Date: " + date;
+                console.log(eachDay[i].dt_txt);
+                cityName.textContent = inputEl;
+                console.log(inputEl)
 
+                weatherCard.setAttribute("class", "weather-cards");
+                temp.setAttribute("class", "weather-temp text-detail");
+                time.setAttribute("class", "text-detail");
+                cityName.setAttribute("class", "text-detail");
 
                 //appended elements to the div
                 cardBox.append(weatherCard);
+                weatherCard.append(cityName);
+                weatherCard.append(time);
                 weatherCard.append(dayIcon);
                 weatherCard.append(temp);
-                weatherCard.append(time);
-
                 
+
             }
 
-            
-            
-
         })
-}
+    }
 
 // lastSearchedCity()
 
